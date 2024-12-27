ea6bace481f61ca0a42dacb029fc9646c76a1d3a
Abdelrhman Khaled <127624610+Abdellrhman-Khaled@users.noreply.github.com>
2024-12-27 21:58:09 +0200

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -49,8 +49,6 @@ s = [
          [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]
       ]
 
-# Constants -------------Copyright goes to https://github.com/Abdellrhman-Khaled ---------------------------------------
-
 
 ShiftingTable=[1, 1, 2, 2, 
             2, 2, 2, 2, 
@@ -88,7 +86,6 @@ ExpantionPerm=[32, 1, 2, 3, 4, 5,
             24, 25, 26, 27, 28, 29,
             28, 29, 30, 31, 32, 1]
 
-# Constants -------------Copyright goes to https://github.com/Abdellrhman-Khaled ---------------------------------------
 
 
 PermTable=[16, 7, 20, 21, 29, 12, 28, 17,
